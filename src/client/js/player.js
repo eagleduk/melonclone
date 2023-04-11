b867721fdc72e4b9e116182ac80f891103e17925
SJ.Lee <eagleduk@gmail.com>
2023-04-11 22:48:45 +0900

    Assignment 04
    스타일 작업

--- src/client/js/player.js
+++ src/client/js/player.js
@@ -1,44 +1,107 @@
-let player;
-document.querySelectorAll("main li.song-item").forEach((element) => {
-  element.addEventListener("click", (e) => {
-    const { id } = element.dataset;
-    const playerFrame = document.querySelector("#player");
-    if (playerFrame.dataset.id === id) return;
-    playerFrame.classList.add("show");
-    playerFrame.classList.remove("hide");
-    playerFrame.dataset.id = id;
+window.YT.ready(() => {
+  const player = new YT.Player("yt-player", {
+    // height: "360",
+    width: "500px",
+    //videoId: id,
+    playerVars: {
+      // controls: 0,
+      rel: 0,
+      fs: 0,
+      modestbranding: 1,
+    },
+  });
+  document.querySelectorAll("main li.song-item").forEach((element) => {
+    element.addEventListener("click", (e) => {
+      const { id } = element.dataset;
+      const playerFrame = document.querySelector("#player");
+      if (playerFrame.dataset.id === id) return;
+      playerFrame.classList.add("show");
+      playerFrame.classList.remove("hide");
+      playerFrame.dataset.id = id;
 
-    if (player) {
-      player.stopVideo();
-      player.destroy();
-      player = null;
-    }
-    player = new YT.Player("yt-player", {
-      height: "360",
-      width: "500px",
-      videoId: id,
-      playerVars: {
-        // controls: 0,
-        rel: 0,
-        fs: 0,
-        modestbranding: 1,
-      },
+      if (player) {
+        player.loadVideoById(id);
+      }
     });
+  });
+
+  document.getElementById("player-slider").addEventListener("click", (e) => {
+    const playerFrame = document.querySelector("#player");
+
+    if (playerFrame.classList.length === 0) {
+      playerFrame.classList.add("show");
+    } else {
+      playerFrame.classList.toggle("hide");
+      playerFrame.classList.toggle("show");
+      playerFrame.dataset.id = Date.now();
+      //   document.querySelector("#player").classList.add("hide");
+      if (player) {
+        // player.stopVideo();
+        // player.destroy();
+      }
+    }
   });
 });
 
-document.getElementById("player-close").addEventListener("click", (e) => {
-  const playerFrame = document.querySelector("#player");
-  playerFrame.classList.remove("show");
-  playerFrame.dataset.id = Date.now();
-  //   document.querySelector("#player").classList.add("hide");
-  player.stopVideo();
-  player.destroy();
-  player = null;
-  setTimeout(() => {
-    document.querySelector("#player").classList.add("hide");
-  }, 700);
-});
+// let player;
+// document.querySelectorAll("main li.song-item").forEach((element) => {
+//   element.addEventListener("click", (e) => {
+//     const { id } = element.dataset;
+//     const playerFrame = document.querySelector("#player");
+//     if (playerFrame.dataset.id === id) return;
+//     playerFrame.classList.add("show");
+//     playerFrame.classList.remove("hide");
+//     playerFrame.dataset.id = id;
+
+//     if (player) {
+//       player.stopVideo();
+//       player.destroy();
+//       player = null;
+//     }
+//     player = new YT.Player("yt-player", {
+//       // height: "360",
+//       width: "500px",
+//       //videoId: id,
+//       playerVars: {
+//         // controls: 0,
+//         rel: 0,
+//         fs: 0,
+//         modestbranding: 1,
+//       },
+//     });
+//   });
+// });
+
+// document.getElementById("player-close").addEventListener("click", (e) => {
+//   const playerFrame = document.querySelector("#player");
+//   playerFrame.classList.remove("show");
+//   playerFrame.dataset.id = Date.now();
+//   document.querySelector("#player").classList.add("hide");
+//   player.stopVideo();
+//   player.destroy();
+//   player = null;
+//   setTimeout(() => {
+//     document.querySelector("#player").classList.add("hide");
+//   }, 700);
+// });
+
+// document.getElementById("player-slider").addEventListener("click", (e) => {
+//   const playerFrame = document.querySelector("#player");
+
+//   if (playerFrame.classList.length === 0) {
+//     playerFrame.classList.add("show");
+//   } else {
+//     playerFrame.classList.toggle("hide");
+//     playerFrame.classList.toggle("show");
+//     playerFrame.dataset.id = Date.now();
+//     //   document.querySelector("#player").classList.add("hide");
+//     if (player) {
+//       player.stopVideo();
+//       player.destroy();
+//       player = null;
+//     }
+//   }
+// });
 
 //player.playVideo()
 //player.pauseVideo()
--- src/controllers/root.controller.js
+++ src/controllers/root.controller.js
@@ -2,5 +2,9 @@ import Song from "../models/song.model";
 
 export const home = async (req, res) => {
   const list = await Song.find().sort({ "statistics.viewCount": -1 });
-  return res.render("home", { pageTitle: "Home", list: list });
+  return res.render("home", {
+    pageTitle: "Home",
+    siteName: "Melong Music",
+    list: list,
+  });
 };
