e56739b4c9ef5d8a9f2732920760e4dd4cdd41ea
SJ.Lee <eagleduk@gmail.com>
2023-04-17 23:16:14 +0900

    플레이 리스트 표현
    플레이 리스트 표현 및 하이라이트
    TODO: 버튼 및 플레이 리스트 스타일

--- src/client/js/player.js
+++ src/client/js/player.js
@@ -1,15 +1,23 @@
+const playerFrame = document.querySelector("#player");
+const playerListElement = document.querySelector("#player-list");
+
 globalThis.playLists = [];
 globalThis.playIndex = -1;
 
 function onStateChange(event) {
   if (event.data === 0) {
     if (globalThis.playIndex < globalThis.playLists.length - 1) {
+      playerListElement.children[globalThis.playIndex].classList.remove(
+        "song-highlight"
+      );
       globalThis.playIndex += 1;
+      playerListElement.children[globalThis.playIndex].classList.add(
+        "song-highlight"
+      );
       const id = globalThis.playLists[globalThis.playIndex];
       event.target.cueVideoById({
         videoId: id,
       });
-      const playerFrame = document.querySelector("#player");
       playerFrame.dataset.id = id;
     }
   } else if (event.data === 5) {
@@ -18,14 +26,9 @@ function onStateChange(event) {
 }
 
 window.YT.ready(() => {
-  const playerFrame = document.querySelector("#player");
-
   const player = new YT.Player("yt-player", {
-    // height: "360",
     width: "500px",
-    //videoId: id,
     playerVars: {
-      // controls: 0,
       rel: 0,
       fs: 0,
       modestbranding: 1,
@@ -36,24 +39,30 @@ window.YT.ready(() => {
     },
   });
 
-  globalThis.player = player;
-
   document
     .querySelectorAll("main li.song-item button.play-song")
     .forEach((element) => {
       element.addEventListener("click", (e) => {
-        const { id } = e.target.dataset;
+        const { id, title } = e.target.dataset;
 
         if (playerFrame.dataset.id === id) return;
         playerFrame.classList.add("show");
         playerFrame.classList.remove("hide");
         playerFrame.dataset.id = id;
-
+        playerListElement.children[globalThis.playIndex]?.classList.remove(
+          "song-highlight"
+        );
         player.cueVideoById({
           videoId: id,
         });
         globalThis.playIndex = 0;
         globalThis.playLists.unshift(id);
+
+        const li = document.createElement("li");
+        li.className = "song-highlight";
+        li.dataset.id = id;
+        li.textContent = title;
+        playerListElement.prepend(li);
       });
     });
 
@@ -61,13 +70,11 @@ window.YT.ready(() => {
     .querySelectorAll("main li.song-item button.add-song")
     .forEach((element) => {
       element.addEventListener("click", (e) => {
-        const { id } = e.target.dataset;
-        // player.cueVideoById({
-        //   index: 0,
-        //   list: id,
-        // })
-        if (globalThis.playLists.indexOf(id) < 0) globalThis.playLists.push(id);
-        if (globalThis.playLists.length === 1) {
+        const { id, title } = e.target.dataset;
+        if (globalThis.playLists.indexOf(id) > -1) return;
+        globalThis.playIndex = 0;
+        globalThis.playLists.push(id);
+        if (globalThis.playLists.length === 1 && false) {
           playerFrame.classList.add("show");
           playerFrame.classList.remove("hide");
           playerFrame.dataset.id = id;
@@ -75,23 +82,12 @@ window.YT.ready(() => {
             videoId: id,
           });
         }
+        const li = document.createElement("li");
+        li.dataset.id = id;
+        li.textContent = title;
+        playerListElement.append(li);
       });
     });
-
-  // document.querySelectorAll("main li.song-item").forEach((element) => {
-  //   element.addEventListener("click", (e) => {
-  //     const { id } = element.dataset;
-  //     const playerFrame = document.querySelector("#player");
-  //     if (playerFrame.dataset.id === id) return;
-  //     playerFrame.classList.add("show");
-  //     playerFrame.classList.remove("hide");
-  //     playerFrame.dataset.id = id;
-
-  //     if (player) {
-  //       player.loadVideoById(id);
-  //     }
-  //   });
-  // });
 
   document.getElementById("player-slider").addEventListener("click", (e) => {
     if (playerFrame.classList.length === 0) {
@@ -100,77 +96,67 @@ window.YT.ready(() => {
       playerFrame.classList.toggle("hide");
       playerFrame.classList.toggle("show");
       playerFrame.dataset.id = Date.now();
-      //   document.querySelector("#player").classList.add("hide");
-      if (player) {
-        // player.stopVideo();
-        // player.destroy();
-      }
     }
   });
 
-  // TODO: 플레이어 버튼 컨트롤
+  document.querySelector("button#prev-song").addEventListener("click", (e) => {
+    if (
+      (globalThis.playIndex === -1 && globalThis.playLists.length === 0) ||
+      globalThis.playLists.length === 1
+    )
+      return;
+
+    playerListElement.children[globalThis.playIndex].classList.remove(
+      "song-highlight"
+    );
+
+    globalThis.playIndex =
+      globalThis.playIndex === 0
+        ? globalThis.playLists.length - 1
+        : globalThis.playIndex - 1;
+    playerListElement.children[globalThis.playIndex].classList.add(
+      "song-highlight"
+    );
+
+    const id = globalThis.playLists[globalThis.playIndex];
+    playerFrame.dataset.id = id;
+
+    player.cueVideoById({
+      videoId: id,
+    });
+  });
+
+  document.querySelector("button#pause-song").addEventListener("click", (e) => {
+    player.pauseVideo();
+  });
+
+  document.querySelector("button#play-song").addEventListener("click", (e) => {
+    if (globalThis.playIndex === -1 && globalThis.playLists.length === 0)
+      return;
+    player.playVideo();
+  });
+
+  document.querySelector("button#next-song").addEventListener("click", (e) => {
+    if (
+      (globalThis.playIndex === -1 && globalThis.playLists.length === 0) ||
+      globalThis.playLists.length === 1
+    )
+      return;
+    playerListElement.children[globalThis.playIndex].classList.remove(
+      "song-highlight"
+    );
+    globalThis.playIndex =
+      globalThis.playIndex === globalThis.playLists.length - 1
+        ? 0
+        : globalThis.playIndex + 1;
+    playerListElement.children[globalThis.playIndex].classList.add(
+      "song-highlight"
+    );
+    const id = globalThis.playLists[globalThis.playIndex];
+    playerFrame.dataset.id = id;
+
+    player.cueVideoById({
+      videoId: id,
+    });
+  });
 });
-
-// let player;
-// document.querySelectorAll("main li.song-item").forEach((element) => {
-//   element.addEventListener("click", (e) => {
-//     const { id } = element.dataset;
-//     const playerFrame = document.querySelector("#player");
-//     if (playerFrame.dataset.id === id) return;
-//     playerFrame.classList.add("show");
-//     playerFrame.classList.remove("hide");
-//     playerFrame.dataset.id = id;
-
-//     if (player) {
-//       player.stopVideo();
-//       player.destroy();
-//       player = null;
-//     }
-//     player = new YT.Player("yt-player", {
-//       // height: "360",
-//       width: "500px",
-//       //videoId: id,
-//       playerVars: {
-//         // controls: 0,
-//         rel: 0,
-//         fs: 0,
-//         modestbranding: 1,
-//       },
-//     });
-//   });
-// });
-
-// document.getElementById("player-close").addEventListener("click", (e) => {
-//   const playerFrame = document.querySelector("#player");
-//   playerFrame.classList.remove("show");
-//   playerFrame.dataset.id = Date.now();
-//   document.querySelector("#player").classList.add("hide");
-//   player.stopVideo();
-//   player.destroy();
-//   player = null;
-//   setTimeout(() => {
-//     document.querySelector("#player").classList.add("hide");
-//   }, 700);
-// });
-
-// document.getElementById("player-slider").addEventListener("click", (e) => {
-//   const playerFrame = document.querySelector("#player");
-
-//   if (playerFrame.classList.length === 0) {
-//     playerFrame.classList.add("show");
-//   } else {
-//     playerFrame.classList.toggle("hide");
-//     playerFrame.classList.toggle("show");
-//     playerFrame.dataset.id = Date.now();
-//     //   document.querySelector("#player").classList.add("hide");
-//     if (player) {
-//       player.stopVideo();
-//       player.destroy();
-//       player = null;
-//     }
-//   }
-// });
-
-//player.playVideo()
-//player.pauseVideo()
-//player.stopVideo()
